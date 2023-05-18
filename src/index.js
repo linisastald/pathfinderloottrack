b2719874b9f7295255348357ca87e08ccb7827f0
Daniel <git@danielkempson.com>
2023-05-18 00:00:08 -0400

    a

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   )
