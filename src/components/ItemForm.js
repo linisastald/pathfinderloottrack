adcddc51c26bdf642bf48dddba3c6adc51cbc7fd
Daniel <git@danielkempson.com>
2023-05-19 00:18:56 -0400

    a

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -47,42 +47,43 @@ function ItemForm() {
     };
 
     return (
-        <form onSubmit={handleSubmit} style={formStyle}>
+<form onSubmit={handleSubmit} style={formStyle}>
+    <label>
+        Session Date:
+        <input type="date" tabIndex="1" value={sessionDate} onChange={e => setSessionDate(e.target.value)} />
+    </label>
+    {items.map((item, index) => (
+        <div key={index}>
             <label>
-                Session Date:
-                <input type="date" value={sessionDate} onChange={e => setSessionDate(e.target.value)} />
+                Item Name:
+                <input type="text" tabIndex={`${2 + index}`} style={{width: '200px'}} value={item.itemName} onChange={e => updateItem(index, 'itemName', e.target.value)} />
             </label>
-            {items.map((item, index) => (
-                <div key={index}>
-                    <label>
-                        Item Name:
-                        <input type="text" style={{width: '200px'}} value={item.itemName} onChange={e => updateItem(index, 'itemName', e.target.value)} />
-                    </label>
-                    <label>
-                        Quantity:
-                        <input type="number" style={{width: '50px'}} value={item.quantity} onChange={e => updateItem(index, 'quantity', Number(e.target.value))} />
-                    </label>
-                    <label>
-                        Unidentified:
-                        <input type="checkbox" checked={item.unidentified} onChange={e => updateItem(index, 'unidentified', e.target.checked)} />
-                    </label>
-                    <label>
-                        Type:
-                        <select value={item.itemType} onChange={e => updateItem(index, 'itemType', e.target.value)}>
-                            {types.map(type => <option key={type} value={type}>{type}</option>)}
-                        </select>
-                    </label>
-                    <label>
-                        Size:
-                        <select value={item.size} onChange={e => updateItem(index, 'size', e.target.value)}>
-                            {sizes.map(s => <option key={s} value={s}>{s}</option>)}
-                        </select>
-                    </label>
-                </div>
-            ))}
-            <button type="button" onClick={addItem}>Add Item</button>
-            <button type="submit">Submit</button>
-        </form>
+            <label>
+                Quantity:
+                <input type="number" tabIndex={`${2 + items.length + index}`} style={{width: '50px'}} value={item.quantity} onChange={e => updateItem(index, 'quantity', Number(e.target.value))} />
+            </label>
+            <label>
+                Unidentified:
+                <input type="checkbox" tabIndex={`${2 + items.length * 2 + index}`} checked={item.unidentified} onChange={e => updateItem(index, 'unidentified', e.target.checked)} />
+            </label>
+            <label>
+                Type:
+                <select tabIndex={`${2 + items.length * 3 + index}`} value={item.itemType} onChange={e => updateItem(index, 'itemType', e.target.value)}>
+                    {types.map(type => <option key={type} value={type}>{type}</option>)}
+                </select>
+            </label>
+            <label>
+                Size:
+                <select tabIndex={`${2 + items.length * 4 + index}`} value={item.size} onChange={e => updateItem(index, 'size', e.target.value)}>
+                    {sizes.map(s => <option key={s} value={s}>{s}</option>)}
+                </select>
+            </label>
+        </div>
+    ))}
+    <button type="button" tabIndex={`${2 + items.length * 5}`} onClick={addItem}>Add Item</button>
+    <button type="submit" tabIndex={`${3 + items.length * 5}`}>Submit</button>
+</form>
+
     );
 }
 
