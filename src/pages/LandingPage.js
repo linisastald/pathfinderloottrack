15781ab21bc3c01a1f3cc9cdf7f66ccd36a046e3
Daniel <git@danielkempson.com>
2023-05-18 23:18:42 -0400

    a

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -6,14 +6,14 @@ const LandingPage = () => {
     <div className="landing-page">
       <h1>Welcome to Pathfinder Party Management</h1>
       <div className="links">
-        <Link to="/item-input">Item Input</Link>
-        <Link to="/gold-input">Gold Input</Link>
-        <Link to="/gold-transactions">Gold Transactions</Link>
-        <Link to="/party-loot">Party Loot</Link>
-        <Link to="/party-owned-items">Party Owned Items</Link>
-        <Link to="/kept-items">Kept Items</Link>
-        <Link to="/sold-items">Sold Items</Link>
-        <Link to="/given-away-items">Given Away Items</Link>
+        <Link to="/item-input"><button>Item Input</button></Link>
+        <Link to="/gold-input"><button>Gold Input</button></Link>
+        <Link to="/gold-transactions"><button>Gold Transactions</button></Link>
+        <Link to="/party-loot"><button>Party Loot</button></Link>
+        <Link to="/party-owned-items"><button>Party Owned Items</button></Link>
+        <Link to="/kept-items"><button>Kept Items</button></Link>
+        <Link to="/sold-items"><button>Sold Items</button></Link>
+        <Link to="/given-away-items"><button>Given Away Items</button></Link>
       </div>
     </div>
   );
