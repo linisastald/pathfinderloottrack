cc94f3c1ac8fc5fc13640b80699edce86c4f7def
Daniel <git@danielkempson.com>
2023-05-18 23:27:22 -0400

    a

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-function App() {
+import LandingPage from './pages/LandingPage';
+import ItemForm from './components/ItemForm';
+import GoldForm from './components/GoldForm';
+import GoldTransactions from './components/GoldTransactions';
+import PartyLoot from './components/PartyLoot';
+import PartyOwnedItems from './components/PartyOwnedItems';
+import KeptItems from './components/KeptItems';
+import SoldItems from './components/SoldItems';
+import GivenAwayItems from './components/GivenAwayItems';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route path="/item-input" component={ItemForm} />
+        <Route path="/gold-input" component={GoldForm} />
+        <Route path="/gold-transactions" component={GoldTransactions} />
+        <Route path="/party-loot" component={PartyLoot} />
+        <Route path="/party-owned-items" component={PartyOwnedItems} />
+        <Route path="/kept-items" component={KeptItems} />
+        <Route path="/sold-items" component={SoldItems} />
+        <Route path="/given-away-items" component={GivenAwayItems} />
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -6,14 +6,14 @@ const LandingPage = () => {
     <div className="landing-page">
       <h1>Welcome to Pathfinder Party Management</h1>
       <div className="links">
-        <Link to="./components/ItemForm"><button>Item Input</button></Link>
-        <Link to="./components/GoldForm"><button>Gold Input</button></Link>
-        <Link to="./components/GoldTransactions"><button>Gold Transactions</button></Link>
-        <Link to="./components/PartyLoot"><button>Party Loot</button></Link>
-        <Link to="./components/PartyOwnedItems"><button>Party Owned Items</button></Link>
-        <Link to="./components/KeptItems"><button>Kept Items</button></Link>
-        <Link to="./components/SoldItems"><button>Sold Items</button></Link>
-        <Link to="./components/GivenAwayItems"><button>Given Away Items</button></Link>
+        <Link to="/item-input"><button>Item Input</button></Link>
+        <Link to="/gold-input"><button>Gold Input</button></Link>
+        <Link to="/gold-transactions"><button>Gold Transactions</button></Link>
+        <Link to="/party-loot"><button>Party Loot</button></Link>
+        <Link to="/party-owned-items"><button>Party Owned Items</button></Link>
+        <Link to="/kept-items"><button>Kept Items</button></Link>
+        <Link to="/sold-items"><button>Sold Items</button></Link>
+        <Link to="/given-away-items"><button>Given Away Items</button></Link>
       </div>
     </div>
   );
