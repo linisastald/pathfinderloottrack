f52a4d2d35f402ce9032099b6016b0138e33845f
Daniel <git@danielkempson.com>
2023-05-18 23:39:58 -0400

    a

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './pages/Home.js';
 import LandingPage from './pages/LandingPage.js';
 import reportWebVitals from './reportWebVitals';
 import ItemForm from './components/ItemForm';
 import './App.css';
-import GoldForm from "./components/GoldForm";
-import GoldTransactions from "./components/GoldTransactions";
-import PartyLoot from "./components/PartyLoot";
-import PartyOwnedItems from "./components/PartyOwnedItems";
-import KeptItems from "./components/KeptItems";
-import SoldItems from "./components/SoldItems";
-import GivenAwayItems from "./components/GivenAwayItems";
+
 
 export default function App() {
   return (
@@ -20,13 +13,6 @@ export default function App() {
       <Routes>
         <Route path="/" component={LandingPage} />
         <Route path="/item-input" component={ItemForm} />
-        <Route path="/gold-input" component={GoldForm} />
-        <Route path="/gold-transactions" component={GoldTransactions} />
-        <Route path="/party-loot" component={PartyLoot} />
-        <Route path="/party-owned-items" component={PartyOwnedItems} />
-        <Route path="/kept-items" component={KeptItems} />
-        <Route path="/sold-items" component={SoldItems} />
-        <Route path="/given-away-items" component={GivenAwayItems} />
       </Routes>
     </BrowserRouter>
   )
