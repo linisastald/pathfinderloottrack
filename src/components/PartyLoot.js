53e5baf2d10c9cadd1f42f9475b5c13aada37c9e
Daniel <git@danielkempson.com>
2023-05-19 13:11:46 -0400

    a

--- src/components/PartyLoot.js
+++ src/components/PartyLoot.js
@@ -1,20 +1,11 @@
-import React, {useEffect, useState} from 'react';
-import apiService from '../../../loottracker/frontend/src/services/apiService';
+import React, {useState} from 'react';
 
 function PartyLoot() {
-    const [items, setItems] = useState([]);
-
-    useEffect(() => {
-        const fetchItems = async () => {
-            try {
-                const itemsData = await apiService.getItems();
-                setItems(itemsData);
-            } catch (error) {
-                console.error('Error fetching items:', error);
-            }
-        };
-        fetchItems();
-    }, []);
+    const [items, setItems] = useState([
+        {id: 1, session_date: '2023-05-01', quantity: 2, item_name: 'Sword', unidentified: false, item_type: 'Weapon', size: 'Medium'},
+        {id: 2, session_date: '2023-05-03', quantity: 1, item_name: 'Ring of Protection', unidentified: true, item_type: 'Magic', size: 'Small'},
+        {id: 3, session_date: '2023-05-07', quantity: 3, item_name: 'Potion', unidentified: false, item_type: 'Gear', size: 'Small'},
+    ]);
 
     return (
         <div>
