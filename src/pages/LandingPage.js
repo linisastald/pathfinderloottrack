9234b1f4b2a70f49557a2b4739bbe795f5a5d755
Daniel <git@danielkempson.com>
2023-05-18 23:21:04 -0400

    a

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -6,14 +6,14 @@ const LandingPage = () => {
     <div className="landing-page">
       <h1>Welcome to Pathfinder Party Management</h1>
       <div className="links">
-        <Link to="./components/item-input"><button>Item Input</button></Link>
-        <Link to="./components/gold-input"><button>Gold Input</button></Link>
-        <Link to="./components/gold-transactions"><button>Gold Transactions</button></Link>
-        <Link to="./components/party-loot"><button>Party Loot</button></Link>
-        <Link to="./components/party-owned-items"><button>Party Owned Items</button></Link>
-        <Link to="./components/kept-items"><button>Kept Items</button></Link>
-        <Link to="./components/sold-items"><button>Sold Items</button></Link>
-        <Link to="./components/given-away-items"><button>Given Away Items</button></Link>
+        <Link to="./components/ItemForm"><button>Item Input</button></Link>
+        <Link to="./components/GoldForm"><button>Gold Input</button></Link>
+        <Link to="./components/GoldTransactions"><button>Gold Transactions</button></Link>
+        <Link to="./components/PartyLoot"><button>Party Loot</button></Link>
+        <Link to="./components/PartyOwnedItems"><button>Party Owned Items</button></Link>
+        <Link to="./components/KeptItems"><button>Kept Items</button></Link>
+        <Link to="./components/SoldItems"><button>Sold Items</button></Link>
+        <Link to="./components/GivenAwayItems"><button>Given Away Items</button></Link>
       </div>
     </div>
   );
