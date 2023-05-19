7d1c0385ce6e6da9bc6feba985bf7a4bf19ef551
Daniel <git@danielkempson.com>
2023-05-18 23:39:06 -0400

    a

--- src/index.js
+++ src/index.js
@@ -6,12 +6,27 @@ import LandingPage from './pages/LandingPage.js';
 import reportWebVitals from './reportWebVitals';
 import ItemForm from './components/ItemForm';
 import './App.css';
+import GoldForm from "./components/GoldForm";
+import GoldTransactions from "./components/GoldTransactions";
+import PartyLoot from "./components/PartyLoot";
+import PartyOwnedItems from "./components/PartyOwnedItems";
+import KeptItems from "./components/KeptItems";
+import SoldItems from "./components/SoldItems";
+import GivenAwayItems from "./components/GivenAwayItems";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<ItemForm />} />
+        <Route exact path="/" component={LandingPage} />
+        <Route path="/item-input" component={ItemForm} />
+        <Route path="/gold-input" component={GoldForm} />
+        <Route path="/gold-transactions" component={GoldTransactions} />
+        <Route path="/party-loot" component={PartyLoot} />
+        <Route path="/party-owned-items" component={PartyOwnedItems} />
+        <Route path="/kept-items" component={KeptItems} />
+        <Route path="/sold-items" component={SoldItems} />
+        <Route path="/given-away-items" component={GivenAwayItems} />
       </Routes>
     </BrowserRouter>
   )
