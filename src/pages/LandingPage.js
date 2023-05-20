a910733c73eceb80cd436f8560b85e7b4d9269a9
Daniel <git@danielkempson.com>
2023-05-19 21:55:26 -0400

    a

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,22 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import PartyLoot from './PartyLoot';
+import GoldTransactions from './GoldTransactions';
+import ItemInput from './ItemInput';
+import GoldInput from './GoldInput';
+import PartyOwnedItems from './PartyOwnedItems';
+import KeptItems from './KeptItems';
+import SoldItems from './SoldItems';
+import GivenAwayItems from './GivenAwayItems';
 
 const LandingPage = () => {
-  return (
-    <div className="landing-page">
-      <h1>Welcome to Pathfinder Party Management</h1>
-      <div className="links">
-        <Link to="/item-input"><button>Item Input</button></Link>
-        <Link to="/gold-input"><button>Gold Input</button></Link>
-        <Link to="/gold-transactions"><button>Gold Transactions</button></Link>
-        <Link to="/party-loot"><button>Party Loot</button></Link>
-        <Link to="/party-owned-items"><button>Party Owned Items</button></Link>
-        <Link to="/kept-items"><button>Kept Items</button></Link>
-        <Link to="/sold-items"><button>Sold Items</button></Link>
-        <Link to="/given-away-items"><button>Given Away Items</button></Link>
-      </div>
-    </div>
-  );
+    const [activeComponent, setActiveComponent] = useState('');
+
+    const renderComponent = (componentName) => {
+        switch(componentName) {
+            case 'ItemInput':
+                return <ItemInput />;
+            case 'GoldInput':
+                return <GoldInput />;
+            case 'GoldTransactions':
+                return <GoldTransactions />;
+            case 'PartyLoot':
+                return <PartyLoot />;
+            case 'PartyOwnedItems':
+                return <PartyOwnedItems />;
+            case 'KeptItems':
+                return <KeptItems />;
+            case 'SoldItems':
+                return <SoldItems />;
+            case 'GivenAwayItems':
+                return <GivenAwayItems />;
+            default:
+                return null;
+        }
+    };
+
+    return (
+        <div className="landing-page">
+            <h1>Welcome to Pathfinder Party Management</h1>
+            <div className="links">
+                <button onClick={() => setActiveComponent('ItemInput')}>Item Input</button>
+                <button onClick={() => setActiveComponent('GoldInput')}>Gold Input</button>
+                <button onClick={() => setActiveComponent('GoldTransactions')}>Gold Transactions</button>
+                <button onClick={() => setActiveComponent('PartyLoot')}>Party Loot</button>
+                <button onClick={() => setActiveComponent('PartyOwnedItems')}>Party Owned Items</button>
+                <button onClick={() => setActiveComponent('KeptItems')}>Kept Items</button>
+                <button onClick={() => setActiveComponent('SoldItems')}>Sold Items</button>
+                <button onClick={() => setActiveComponent('GivenAwayItems')}>Given Away Items</button>
+            </div>
+            {renderComponent(activeComponent)}
+        </div>
+    );
 };
 
 export default LandingPage;
