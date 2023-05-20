6613e017d7ea95013e471d998098f56b28ec0598
Daniel <git@danielkempson.com>
2023-05-20 10:58:46 -0400

    a

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -3,7 +3,7 @@ import PartyLoot from '../components/PartyLoot';
 import GoldTransactions from '../components/GoldTransactions';
 import ItemForm from '../components/ItemForm';
 import GoldForm from '../components/GoldForm';
-//import PartyOwnedItems from '../components/PartyOwnedItems';
+import PartyOwnedItems from '../components/PartyOwnedItems';
 //import KeptItems from '../components/KeptItems';
 //import SoldItems from '../components/SoldItems';
 //import GivenAwayItems from '../components/GivenAwayItems';
@@ -22,7 +22,7 @@ const LandingPage = () => {
             case 'PartyLoot':
                 return <PartyLoot />;
             case 'PartyOwnedItems':
-            //    return <PartyOwnedItems />;
+                return <PartyOwnedItems />;
             case 'KeptItems':
             //    return <KeptItems />;
             case 'SoldItems':
