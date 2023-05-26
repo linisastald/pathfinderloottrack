cf86d3072ac2c46bacb3411863938fa468a68912
Daniel <git@danielkempson.com>
2023-05-26 18:06:51 -0400

    partyloot api add

--- src/components/PartyLoot.js
+++ src/components/PartyLoot.js
@@ -1,22 +1,22 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function PartyLoot() {
     const navigate = useNavigate();
-const [items, setItems] = useState([
-    {id: 1, session_date: '2023-05-01', quantity: 2, item_name: 'Sword', unidentified: false, item_type: 'Weapon', size: 'Medium', avg_believed_value: 50, who_appraised: 'John'},
-    {id: 2, session_date: '2023-05-02', quantity: 1, item_name: 'Armor', unidentified: true, item_type: 'Armor', size: 'Large', avg_believed_value: 100, who_appraised: 'Jane'},
-    {id: 3, session_date: '2023-05-03', quantity: 3, item_name: 'Wand', unidentified: false, item_type: 'Magic', size: 'Small', avg_believed_value: 75, who_appraised: 'Bob'},
-    {id: 4, session_date: '2023-05-04', quantity: 1, item_name: 'Axe', unidentified: false, item_type: 'Weapon', size: 'Large', avg_believed_value: 80, who_appraised: 'Alice'},
-    {id: 5, session_date: '2023-05-05', quantity: 5, item_name: 'Cloak', unidentified: true, item_type: 'Gear', size: 'Medium', avg_believed_value: 60, who_appraised: 'John'},
-    {id: 6, session_date: '2023-05-06', quantity: 2, item_name: 'Ring', unidentified: false, item_type: 'Magic', size: 'Tiny', avg_believed_value: 120, who_appraised: 'Jane'},
-    {id: 7, session_date: '2023-05-07', quantity: 4, item_name: 'Helmet', unidentified: true, item_type: 'Armor', size: 'Medium', avg_believed_value: 70, who_appraised: 'Bob'},
-    {id: 8, session_date: '2023-05-08', quantity: 1, item_name: 'Boots', unidentified: false, item_type: 'Gear', size: 'Small', avg_believed_value: 40, who_appraised: 'Alice'},
-    {id: 9, session_date: '2023-05-09', quantity: 3, item_name: 'Amulet', unidentified: false, item_type: 'Magic', size: 'Small', avg_believed_value: 130, who_appraised: 'John'},
-    {id: 10, session_date: '2023-05-10', quantity: 5, item_name: 'Potion', unidentified: true, item_type: 'Magic', size: 'Tiny', avg_believed_value: 25, who_appraised: 'Alice'},
-]);
-
+    const [items, setItems] = useState([]);
     const [selectedItems, setSelectedItems] = useState([]);
+
+    useEffect(() => {
+        async function fetchItems() {
+            const response = await fetch('http://localhost:5000/item');
+            if (response.ok) {
+                const data = await response.json();
+                setItems(data.items);
+            }
+        }
+        fetchItems();
+    }, []);
+
     const handleUpdate = () => {
         navigate(`/item-update/${selectedItems.join(",")}`);
     };
