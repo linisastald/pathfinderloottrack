3e14aef4fab700fdeb6c99413e27e2c71772d1cb
Daniel <git@danielkempson.com>
2023-05-30 16:37:23 -0400

    update PartyLoot.js - button updates

--- src/components/PartyLoot.js
+++ src/components/PartyLoot.js
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from 'react';
-import {useNavigate} from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function PartyLoot() {
     const navigate = useNavigate();
@@ -8,42 +8,15 @@ function PartyLoot() {
     const [characters, setCharacters] = useState([]);
     const [selectedCharacter, setSelectedCharacter] = useState('');
 
-    useEffect(() => {
-        async function fetchItems() {
-            const response = await fetch('http://192.168.0.64:5000/item/status/none');
-            if (response.ok) {
-                const data = await response.json();
-                const items = data.items.map(item => ({
-                    id: item.id,
-                    session_date: item.session_date,
-                    quantity: item.quantity,
-                    item_name: item.name,
-                    unidentified: item.unidentified,
-                    item_type: item.type,
-                    size: item.size,
-                    avg_believed_value: item.cost,
-                    who_appraised: item.who,
-                }));
-
-                const newItems = [];
-
-                items.forEach(item => {
-                    const existingItem = newItems.find(i => i.item_name === item.item_name && i.item_type === item.item_type && i.size === item.size);
-                    if (existingItem) {
-                        existingItem.quantity += item.quantity;
-                        existingItem.session_dates.push(item.session_date);
-                    } else {
-                        newItems.push({
-                            ...item,
-                            session_dates: [item.session_date],
-                        });
-                    }
-                });
-
-                setItems(newItems);
-            }
+    const fetchItems = async () => {
+        const response = await fetch('http://192.168.0.64:5000/item/status/none');
+        if (response.ok) {
+            const data = await response.json();
+            setItems(data.items);
         }
+    };
 
+    useEffect(() => {
         async function fetchCharacters() {
             const response = await fetch('http://192.168.0.64:5000/character');
             if (response.ok) {
@@ -78,6 +51,7 @@ function PartyLoot() {
                 body: JSON.stringify({status, who}),
             });
         }
+        await fetchItems();
     };
 
     return (
@@ -105,11 +79,7 @@ function PartyLoot() {
                             <input type="checkbox" onChange={() => handleSelect(item)}
                                    checked={selectedItems.includes(item.id)}/>
                         </td>
-                        <td style={{border: '1px solid white'}}>
-                            {item.session_dates
-                                ? [...new Set(item.session_dates)].map(date => new Date(date).toLocaleDateString()).join(', ')
-                                : ''}
-                        </td>
+                        <td style={{border: '1px solid white'}}>{item.session_dates}</td>
                         <td style={{border: '1px solid white'}}>{item.quantity}</td>
                         <td style={{border: '1px solid white'}}>{item.item_name}</td>
                         <td style={{border: '1px solid white'}}>{item.unidentified ? 'Yes' : 'No'}</td>
